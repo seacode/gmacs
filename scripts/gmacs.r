1438de4c078806ef080a140d80cb92807b1c6e61
Athol Whitten <awhitten@gmail.com>
2014-01-13 18:50:25 -0900

    Update gmacs.r and add new source file

--- scripts/gmacs.r
+++ scripts/gmacs.r
@@ -1,126 +1,94 @@
 #=========================================================================================================
 #
-#	Gmacs.r : Version 1.0 (November 2013)
-# 	R functions to import content from a Gmacs model run, and produce plots.
+#  Gmacs.r : Version 1.0 (January 2014)
+#  R script: Import content from Gmacs model output; produce plots.
+#  Authors: Athol R. Whitten, James N. Ianelli 
+#  Updated: January 2014
 #
-# 	Returns: A list containing elements of gmacs.rep and/or gmacs.cor,
-# 	formatted as R objects, and optional summary statistics to R console.
+#  Acknowledgements: 'Read' functions based on code developed for ADMB by Steve Martell
+#  Some plotting functions based on code developed for Stock Synthesis by Ian Taylor (r4ss).  
 #
-# 	Plots: Various fits to data, summary statistics, and model functions.
-#
-# 	Author: Athol R. Whitten <whittena@uw.edu>
-# 	and other contributors at https:/github.com/awhitten/gmacs
-#
-# 	Acknowledgements: 'Read' functions based on code developed for ADMB by Steve Martell
-#	Some plotting functions based on code developed for Stock Synthesis by Ian Taylor annd Others (r4ss).
+#  Returns: A list containing elements of gmacs_r.rep
+#  Plots: Various fits to data, summary statistics.
 #
 #=========================================================================================================
 
+# Remove previous R console objects; load required packages:
+rm(list=ls())
 
-# Function [GMOutput]: Wrapper function to read output files, create lists, and get fits to data.
-# --------------------------------------------------------------------------------------------------------
-GMOutput <- function(dir="C:/Dropbox/Github/gmacs/examples",
-		model="model", repfile="gmacs.rep", parfile="gmacs.par", corfile="gmacs.cor", 
-		psvfile="gmacs.psv", covar=TRUE, forecast=FALSE){
+#=======================================
+# Set and load directories/files
+#=======================================
+source('reptoRlist.r')
+windows(record=TRUE)
+#layout(matrix(1:8, 4, 2, byrow = TRUE))
 
-		replist <- GMRead.Rep(model=model)
-		
-		fit <- GMRead.Fit(model=model,covar=covar)
-		replist$fit <- fit
-		
-		#pfn <- file.path(dir,model,psvfile)
-		#if(file.exists(pfn)) replist$post.samp <- GMRead.PSV(pfn)
-		
-		return(replist)
-	}
-
-
-# Function [GMRead.Rep]: Read in a Gmacs report file (gmacs.rep)
-# --------------------------------------------------------------------------------------------------------
-GMRead.Rep <- function(dir="C:/Dropbox/Github/gmacs/examples",
-		model="model", repfile="gmacs.rep"){
-		
-		repfile <- file.path(dir,model,repfile)
-		
-		options(warn=-1)  # Suppress NA message when coercing to double
-		
-		repscan <- scan(repfile, what="character", flush=TRUE, blank.lines.skip=FALSE, quiet=TRUE)
-		na.check <- sapply(as.double(repscan), is.na)
-		vnames <- repscan[na.check] # List of names in scan
-		vnumber <- length(vnames)   # Number of components in scan
-		
-		replist <- list()
-		imatch <- 0
-		for(i in 1:vnumber)
-		{
-			imatch <- match(vnames[i],repscan)
-			if(i!=vnumber) kmatch <- match(vnames[i+1],repscan) else kmatch <- length(repscan)+1 # Get next row
-			
-			value <- NA
-			if(kmatch-imatch == 2) value <- as.double(scan(repfile, skip=imatch ,nlines=1, quiet=TRUE, what=""))
-			if(kmatch-imatch >  2) value <- as.matrix(read.table(repfile, skip=imatch, nrow=kmatch-imatch-1, fill=TRUE))
-
-			if(is.numeric(value)) # Logical test to ensure dealing with numbers
-			{
-				replist[[vnames[i]]] <- value
-			}
-		}
-		options(warn=0)
-		
-		return(replist)
-	}
-
-# Function [GMRead.Fit]: Read in Gmacs parameter and correlation files (gmacs.par, gmacs.cor)
-# --------------------------------------------------------------------------------------------------------
-	
-GMRead.Fit <- function(dir="C:/Dropbox/Github/gmacs/examples",
-		model="model", parfile="gmacs.par", corfile="gmacs.cor", covar=TRUE){
-		
-		parfile <- file.path(dir,model,parfile)
-		corfile <- file.path(dir,model,corfile)
-		
-		fitlist <- list()		
-		
-		parscan <- as.numeric(scan(parfile, what='', n=16, quiet=TRUE)[c(6,11,16)]) 
-		fitlist$nopar <- as.integer(parscan[1]) 
-		fitlist$nlogl <- parscan[2] 
-		fitlist$maxgrad <- parscan[3] 
-		
-		if(covar==TRUE){
-			corlines <- readLines(corfile) 
-			fitlist$npar <- length(corlines)-2 
-			fitlist$logDetHess <- as.numeric(strsplit(corlines[1], '=')[[1]][2]) 
-			
-			sublines <- lapply(strsplit(corlines[1:fitlist$npar+2], ' '),function(x)x[x!='']) 
-			fitlist$names <- unlist(lapply(sublines,function(x)x[2])) 
-			fitlist$est <- as.numeric(unlist(lapply(sublines,function(x)x[3]))) 
-			fitlist$std <- as.numeric(unlist(lapply(sublines,function(x)x[4]))) 
-			fitlist$cor <- matrix(NA, fitlist$npar, fitlist$npar) 
-			
-			corvec <- unlist(sapply(1:length(sublines), function(i)sublines[[i]][5:(4+i)])) 
-			fitlist$cor[upper.tri(fitlist$cor, diag=TRUE)] <- as.numeric(corvec) 
-			fitlist$cor[lower.tri(fitlist$cor)] <- t(fitlist$cor)[lower.tri(fitlist$cor)] 
-			fitlist$cov <- fitlist$cor*(fitlist$std%o%fitlist$std)
-		}
-		
-		return(fitlist)
-	}
-
-# Function [GMRead.PSV]: Read binary output from Gmacs -mcsave option (gmacs.psv)
+# Read and assign gmacs_r.rep output file:
+gmout <- reptoRlist('gmacs_r.rep')
+names(gmout)
+summary(gmout)
 # --------------------------------------------------------------------------------------------------------
 
-GMRead.PSV <- function(dir="C:/Dropbox/Github/gmacs/examples",
-		model="model", psvfile="gmacs.psv", nsamples=10000){
-		
-		psvfile <- file.path(dir,model,psvfile)
+# Plot fits to catch data (main fisheries):
+plot(1, type="n", main="Fit to Catch Data  (Retained Pot Fishery)", xlab="Years", ylab ="Catch (tonnes)", xlim=(c(min(gmout$years),max(gmout$years)+1)), ylim=c(0,max(gmout$catch_biom_obs[2, ])), cex.main=1.2)
+nfleet_ret <- 2
+for(ifleet in nfleet_ret)
+ {
+  abline(h=0)
+  lines(gmout$years, gmout$catch_biom_obs[ifleet, ], type="p", pch=20, col=2, cex=1.2)
+  lines(gmout$years, gmout$catch_biom_pred[ifleet, ], type= "l", col=4, lwd=2)
+  legend("topright", legend=c("Observed","Predicted"), pch=20, col=c(2,4), bty="n")
+ }
+ 
+# Plot fits to catch data (main fisheries):
+plot(1, type="n", main="Fit to Catch Data (Trawl Bycatch)", xlab="Years", ylab ="Catch (tonnes)", xlim=(c(min(gmout$years),max(gmout$years)+1)), ylim=c(0,max(gmout$catch_biom_obs[3, ])), cex.main=1.2)
+nfleet_ret <- 3
+for(ifleet in nfleet_ret)
+{
+  abline(h=0)
+  lines(gmout$years, gmout$catch_biom_obs[ifleet, ], type="p", pch=20, col=2, cex=1.2)
+  lines(gmout$years, gmout$catch_biom_pred[ifleet, ], type= "l", col=4, lwd=2)
+  legend("topright", legend=c("Observed","Predicted"), pch=20, col=c(2,4), bty="n")
+}
 
-		read.file <- file(psvfile, open="rb")
-		num.par <- readBin(read.file, what = integer(), n = 1)
-		mcmc <- readBin(read.file, what = numeric(), n = nopar * nsamples)
-		mcmc <- matrix(mcmc, byrow = TRUE, ncol = nopar)
-		close(read.file)
-		return(mcmc)
-	}
+# Plot fits to survey data:
+plot(1, type="n", main="Fit to survey data", xlab="Years", ylab ="Survey Observation", xlim=(c(min(gmout$years),max(gmout$years)+1)), ylim=c(0,max(c(gmout$survey_num_pred[1, ],gmout$survey_num_obs[1, ]))), cex.main=1.2)
+
+# Survey 1 (NMFS) 
+nsurvey <- 1
+for(isrv in 1:nsurvey)
+{
+  lines(gmout$yr_survey[isrv,], gmout$survey_num_obs[isrv, ], type="p", pch=20, col=isrv, cex=1.2)
+  lines(gmout$yr_survey[isrv,], gmout$survey_num_pred[isrv, ], type= "l", col=isrv+2, lwd=2)
+  legend("topright", legend=c("Observed","Predicted"), pch=20, col=c(isrv,isrv+2), bty='n')
+}
+
+# Survey 2 (BRSF)  
+nsurvey <- 2
+for(isrv in 1:nsurvey)
+{
+  lines(gmout$yr_survey[isrv,], gmout$survey_num_obs[isrv, ], type="p", pch=20, col=isrv, cex=1.2)
+  lines(gmout$yr_survey[isrv,], gmout$survey_num_pred[isrv, ], type= "l", col=isrv+2, lwd=2)
+  legend("topright", legend=c("Observed","Predicted"), pch=20, col=c(isrv,isrv+2), bty='n')
+}
+ 
+# Plot Selectivity      
+plot(1:5,gmout$select_fish_2[43,2:6], type="b", main="Selectivity", xlab="Size Class", ylab ="Selectivity" , pch=19,col="blue",cex.main=1.2)
+for(i in 1:43)
+{
+#  lines(1:5, gmout$select_fish_2[i,2:6])
+  lines(1:5, gmout$select_fish_1[i,2:6],col="salmon")
+  lines(1:5, gmout$select_fish_3[i,2:6],col="grey")
+}
+
+# Plot Natural mortality
+plot(gmout$years,gmout$M, type="b", main="Time-varying Natural Mortality (M)", xlab="Years", ylab ="M", xlim=(c(min(gmout$years),max(gmout$years)+1)), ylim=c(0,max(gmout$M)), pch=19,col="blue",cex.main=1.2)
+
+# Plot Recruitment
+plot(gmout$years,gmout$recruits, type="b", main="Yearly Recruitment", xlab="Years", ylab ="No. of Recruits", xlim=(c(min(gmout$years),max(gmout$years)+1)), ylim=c(0,max(gmout$recruits)), pch=19,col="blue",cex.main=1.2)
+
+# Plot Fishing Mortality Rate
+plot(gmout$exp_rate_2[,1], gmout$exp_rate_2[,2], type="b", main="Exploitation Rate (F)", xlab="Years", ylab ="Rate", xlim=(c(min(gmout$years),max(gmout$years)+1)), ylim=c(0,max(gmout$exp_rate_2[,2])), pch=19,col="red",cex.main=1.2)
 
 #=========================================================================================================
 # EOF
