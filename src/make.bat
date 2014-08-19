cc28b1c494635cc57350faeff191edb482209fae
jimianellil <jim.ianelli@gmail.com>
2014-08-18 23:00:45 -0700

    added batch file for dos/windows facility

---
+++ src/make.bat
@@ -0,0 +1,7 @@
+if NOT EXIST build mkdir build
+if NOT EXIST build\debug mkdir build\debug
+if NOT EXIST build\release mkdir build\release
+call admb -g gmacs.tpl tailcompression.cpp multinomial.cpp nloglike.cpp
+copy gmacs.exe build\debug
+call admb -f gmacs.tpl tailcompression.cpp multinomial.cpp nloglike.cpp
+copy gmacs.exe build\release
