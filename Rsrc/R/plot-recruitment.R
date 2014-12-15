367af1965c88e9ea869466bc5dfb2952f69a403a
Jim Ianelli <jim.ianelli@gmail.com>
2014-12-15 14:00:57 -0800

    added in gmr package directories

---
+++ Rsrc/R/plot-recruitment.R
@@ -0,0 +1,21 @@
+#' Plot predicted recruitment and approximate asymptotic error-bars
+#'
+#'
+#' @param replist List object created by read_admb function
+#' @return Plot of predicted recruitment
+#' @export
+plot_recruitment <- function(replist){
+  A <- replist
+  dfpar   <- data.frame(par=A$fit$names,log_rec=A$fit$est,log_sd=A$fit$std)
+  df      <- subset(dfpar,par=="sd_log_recruits")[,-1]
+  df$year <- A$mod_yrs
+  df$lb   <- exp(df$log_rec - 1.96*df$log_sd)
+  df$ub   <- exp(df$log_rec + 1.96*df$log_sd)
+
+  p <- ggplot(df,aes(x=factor(year),y=exp(log_rec)))
+  p <- p + geom_bar(stat = "identity", alpha=0.4)
+  p <- p + geom_pointrange(aes(factor(year),exp(log_rec),ymax=ub,ymin=lb))
+  p <- p + labs(x="Year",y="Recruitment")
+  pRecruitment <- p + ggtheme
+  return(pRecruitment)
+}
---
+++ Rsrc/gmacs.R
@@ -0,0 +1,42 @@
+#=========================================================================================================
+#
+#  gmr Script for Gmacs: Example for BBRKC Demonstration Model
+#  Authors: Athol Whitten, Jim Ianelli
+#  Info: https://github.com/seacode/gmr
+#
+#=========================================================================================================
+
+# Load gmr package for Gmacs:
+library(gmr)
+
+# Set working directory to that containing Gmacs model results:
+setwd("c:/seacode/gmacs/examples/demo")
+setwd("~/_mymods/seacode/gmacs/examples/demo")
+
+# Set theme for ggplot2 (works for themes classic, minimal, gray, bw):
+set_ggtheme('bw')
+
+# Read report file and create gmacs report object (a list):
+gmrep <- read_admb('gmacs')
+
+# Get plots of interest:
+plot_catch(gmrep)
+plot_catch(gmrep,plot_res=T)
+
+plot_sizecomp(gmrep,which_plots=c(1))
+plot_sizecomp(gmrep)
+plot_sizecomp_res(gmrep, which_plots=c(1))
+
+plot_sizecomp(gmrep,which_plots=c(11))
+plot_sizecomp_res(gmrep)
+plot_sizetransition(gmrep)
+
+plot_selectivity(gmrep)
+plot_recruitment(gmrep)
+plot_ssb(gmrep)
+plot_naturalmortality(gmrep)
+plot_naturalmortality(gmrep)
+
+shiny_gmacs(gmrep)
+
+#=========================================================================================================
