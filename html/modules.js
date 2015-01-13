1d0f478e863dfa0cf66784a953e8d55a2672b39f
Steve Martell <martell.steve@gmail.com>
2015-01-13 09:46:34 -0800

    aUpdates to API

--- html/modules.js
+++ html/modules.js
@@ -6,6 +6,6 @@ var modules =
     [ "Classes defined automatically.", "d5/d3c/group___l_i_b.html", "d5/d3c/group___l_i_b" ],
     [ "DATA_SECTION", "dc/d17/group___d_a_t_a___s_e_c_t_i_o_n.html", "dc/d17/group___d_a_t_a___s_e_c_t_i_o_n" ],
     [ "PARAMETER_SECTION", "d2/dca/group___p_a_r_a_m_e_t_e_r___s_e_c_t_i_o_n.html", "d2/dca/group___p_a_r_a_m_e_t_e_r___s_e_c_t_i_o_n" ],
-    [ "GMACS", "d1/d8d/group___g_m_a_c_s.html", "d1/d8d/group___g_m_a_c_s" ],
-    [ "Selectivities", "dd/d28/group___selectivities.html", "dd/d28/group___selectivities" ]
+    [ "Selectivities", "dd/d28/group___selectivities.html", "dd/d28/group___selectivities" ],
+    [ "GMACS", "d1/d8d/group___g_m_a_c_s.html", "d1/d8d/group___g_m_a_c_s" ]
 ];
