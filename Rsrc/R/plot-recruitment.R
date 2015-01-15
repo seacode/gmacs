f5b29560ecebd7c999038185a9f14247f2380afd
kellijohnson <kellifayejohnson@gmail.com>
2015-01-15 11:52:02 -0800

    Add function to plot multiple recruitments across models

--- Rsrc/R/plot-recruitment.R
+++ Rsrc/R/plot-recruitment.R
@@ -14,3 +14,23 @@ plot_recruitment <- function(replist){
   pRecruitment <- p + ggtheme
   return(pRecruitment)
 }
+
+#' Plot predicted recruitment across model runs
+#'
+#'
+#' @param data A list of multiple objects created by read_admb function
+#' @param modnames A vector of model names included in \code{data}
+#' @return Plot of predicted recruitment compared across models
+#' @author Cole Monnahan Kelli Johnson
+#' @export
+plot_models_recruitment <- function(data, modnames){
+    recs <- lapply(data, get_recruitment)
+    df <- do.call("rbind", Map(cbind, recs, modname = modnames))
+
+  p <- ggplot(df,aes(x=factor(year),y=exp(log_rec), group=modname, colour=modname))
+  p <- p + geom_line(stat = "identity", alpha=0.4)
+  p <- p + geom_pointrange(aes(factor(year),exp(log_rec),ymax=ub,ymin=lb))
+  p <- p + labs(x="Year", y="Recruitment")
+  pRecruitment <- p + ggtheme
+  return(pRecruitment)
+}
