ab4434d96ed63a01732e9673cce52e56197c7f3a
Jim Ianelli <jim.ianelli@gmail.com>
2015-01-27 06:03:32 -0800

    updated plot-catch.R in gmr

--- Rsrc/gmacs.R
+++ Rsrc/gmacs.R
@@ -1,7 +1,7 @@
 #=========================================================================================================
 #
 #  gmr Script for Gmacs: Example for BBRKC Demonstration Model
-#  Authors: Athol Whitten, Jim Ianelli
+#  Authors: Steve Martell, Jim Ianelli, Athol Whitten
 #  Info: https://github.com/seacode/gmr
 #
 #=========================================================================================================
@@ -11,7 +11,6 @@ library(ggplot2)
 library(reshape2)
 library(dplyr)
 library(gmr)
-
 
 # GLOBAL CONSTS
 .OVERLAY  = TRUE
@@ -32,12 +31,13 @@ library(gmr)
 # SOURCE R_SRCIPTS FROM .LIB
 setwd(.PWD)
 for(nm in .RFILES) source(file.path(.LIB, nm), echo=FALSE)
+
 # READ MODEL OUTPUTS FROM .MODELDIR
 fn       <- paste0(.MODELDIR,"gmacs")
 M        <- lapply(fn,read_admb)
-names(M) <- basename(.MODELDIR)
 
 # PLOT ROUTINES
+	plot_catch( M )
 	plot_ssb( M )
 	plot_recruitment( M )
 	plot_recruitment( M[1] )
@@ -47,51 +47,8 @@ names(M) <- basename(.MODELDIR)
 	plot_selectivity( M )
 	plot_sizeComps( M[1], 1 )
 	plot_sizeComps( M, 1 )
-
-# Set working directory to that containing Gmacs model results:
-# setwd("c:/seacode/gmacs/examples/demo")
-# setwd("~/_mymods/seacode/gmacs/examples/demo")
-# setwd("~/_mymods/seacode/gmacs/examples/bbrkc")
-setwd("/Users/stevenmartell1/Documents/CURRENT PROJECTS/GMACS/examples/bbrkc")
-# setwd("c:/Users/Crab2015/gmacs/examples/bbrkc")
-# Set theme for ggplot2 (works for themes classic, minimal, gray, bw):
-set_ggtheme('bw')
-
-# Read report file and create gmacs report object (a list):
-gmrep <- read_admb('gmacs')
-
-# Get plots of interest:
-plot_catch(gmrep)
-plot_growth(gmrep)
-plot_catch(gmrep,plot_res=TRUE)
-names(gmrep$fit)
-plot_growth_inc(gmrep)
-plot_cpue(gmrep)
-
-plot_sizecomp(gmrep,which_plots=c(1))
-plot_sizecomp(gmrep)
-plot_sizecomp_res(gmrep, which_plots=c(1))
-
-
-plot_sizecomp(gmrep,which_plots=c(1))
-plot_sizecomp(gmrep,which_plots=c(2))
-plot_sizecomp(gmrep,which_plots=c(5))
-
-
-plot_sizecomp(gmrep,which_plots=c(7))
-plot_sizecomp(gmrep,which_plots=c(8))
-
-
-
-plot_sizecomp_res(gmrep)
-plot_sizetransition(gmrep)
-
-plot_selectivity(gmrep)
-plot_recruitment(gmrep)
-plot_datarange(gmrep)
-plot_ssb(gmrep)
-plot_naturalmortality(gmrep)
-
-shiny_gmacs(gmrep)
-
+  plot_datarange(M[1])
+  plot_naturalmortality(M[[1]])
+  plot_growth_inc(M[[2]])
+ #shiny_gmacs(gmrep)
 #=========================================================================================================
