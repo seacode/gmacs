398dabf143108d0652ddd98da87b637831fc54ab
Jim Ianelli <jim.ianelli@gmail.com>
2015-01-27 14:55:20 -0800

    added retained curves to selectivity thanks to SJDM

--- Rsrc/R/plot-naturalmortality.R
+++ Rsrc/R/plot-naturalmortality.R
@@ -1,24 +1,37 @@
 #' Plot natural mortality
 #'
-#' @param replist List object created by read_admb function
-#' @return Plot natural mortality over time and size
+#' @param M List object created by read_admb function
+#' @param plt_surface Include a panel with surface over size-time 
+#' @return Plot natural mortality over time (and size)
 #' @export
-plot_naturalmortality <- function(replist){
-  A    <- replist
-  df   <- data.frame(A$M)
-  colnames(df) <- A$mid_points
-  nrow   <- dim(A$M)[1]
-  # Always saves for both sexes???
-  df$sex <- c(rep(1,length=nrow/2),rep(2,length=nrow/2))
-  df$Year <- A$mod_yrs
-  mdf    <- melt(df,id=c("sex","Year"))
+plot_naturalmortality <- function(M,plt_surface=FALSE)
+{
+  n    <- length(M)
+  mdf  <- NULL
+  for (i in 1:n)
+  {
+    A      <- M[[i]]
+    nrow   <- dim(A$M)[1]
+    nsex   <- nrow / length(A$mod_yrs)
+    A$sex <- rep(1,length=nrow/nsex)
+    if (nsex >1) A$sex <- c(A$sex,rep(2,length=nrow/nsex))
+    df <- data.frame(Model=names(M)[i],cbind(A$mod_yrs, .SEX[A$sex+1], M[[i]]$M))
+    colnames(df) <- c("Model","Year","sex",as.character(A$mid_points))
+    mdf    <- melt(df,id=c("Model","sex","Year"))
+  }
   
-  p <- ggplot(mdf,aes(x=Year,y=as.double(variable),z=value))
-  p <- p + geom_tile(aes(fill = value)) 
-  p <- p + stat_contour(geom="polygon", aes(fill=(value)))
-  p <- p + labs(x="Year",y="size bin",fill="M")
-  p <- p + facet_wrap(~sex,scale="free")
-  p2 <- ggplot(mdf,aes(x=Year,y=value))
-  p2 <- p2 + geom_line() + ggtheme + labs(y="Natural mortality")
+  #p <- ggplot(mdf,aes(x=Year,y=as.double(variable),z=value))
+  #p <- p + geom_tile(aes(fill = value)) 
+  #p <- p + stat_contour(geom="polygon", aes(fill=(value)))
+  #p <- p + labs(x="Year",y="size bin",fill="M")
+  #p <- p + facet_wrap(~sex,scale="free")
+  p <- ggplot(mdf,aes(x=Year,y=value))
+  p <- ggplot(mdf)
+  p <- p + geom_line(aes(x=Year,value,col=sex))
+  p <- p + facet_wrap(~Model)
+  p <- p + geom_line() + ggtheme + labs(y="Natural mortality")
+  print(p)
   plot_multiple(p2,p)
+  }
 }
+    
--- Rsrc/R/plot-selectivity.R
+++ Rsrc/R/plot-selectivity.R
@@ -9,15 +9,25 @@
 	mdf <- NULL
 	for(i in 1:n)
 	{
-		df <- data.frame(Model=names(M)[i], M[[i]]$slx_capture)
-		colnames(df) <- c("Model","year","sex","fleet",as.character(M[[i]]$mid_points))
-		df$sex = .SEX[df$sex+1]
+		A  <- M[[i]]
+		# capture
+		df <- data.frame(Model=names(M)[i],
+		         type = .TYPE[1],
+                 M[[i]]$slx_capture)
+		# retained 
+		dr <- data.frame(Model=names(M)[i],
+	             type = .TYPE[2],
+                 M[[i]]$slx_retaind)	
+		colnames(df) <- c("Model","type","year","sex","fleet",as.character(A$mid_points))
+		colnames(dr) <- colnames(df)
+		df$sex   = .SEX[df$sex+1]
 		df$fleet = .FLEET[df$fleet]
-
+		dr$sex   = .SEX[dr$sex+1]
+		dr$fleet = .FLEET[dr$fleet]
 		blkyr <- M[[i]]$slx_control[,12]
 		df    <- filter(df,year %in% blkyr)
-
-		mdf <- rbind(mdf,melt(df,id.var=1:4))
+		dr    <- filter(dr,year %in% blkyr)
+		mdf <- rbind(mdf,melt(df,id.var=1:5),melt(dr,id.var=1:5))
 	}
   return(mdf)  
 }
@@ -28,24 +38,27 @@
 #' @return Plot of selectivity
 #' @author SJD Martell
 #' @export
-plot_selectivity <- function( M ){
+plot_selectivity <- function( M )
+{
 	mdf <- .get_selectivity_df(M)
+
+
 	p <- ggplot(mdf)
 	if(.OVERLAY)
 	{
-		p <- p + geom_line(aes(as.numeric(variable),value,col=Model,linetype=factor(year)))
-		p <- p + facet_wrap(~sex+fleet)
+		p <- p + geom_line(aes(as.numeric(variable),value,col=type,linetype=factor(year)))
+		p <- p + facet_wrap(~Model+sex+fleet)
 	}
 	else
 	{
 		p <- p + geom_line(aes(as.numeric(variable),value,col=sex,linetype=factor(year)))
-		p <- p + facet_wrap(~Model+fleet)
+		p <- p + facet_wrap(~Model+fleet+type)
 	}
 	p <- p + labs(y="Selectivity",
 	              x="Mid point of size class (mm)",
-	              col="Sex",
+	              col="Type",
 	              linetype="Block Year")
 
-  print(p + .THEME)
 
+	print(p + .THEME)
 }
