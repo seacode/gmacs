b41aca4acc17944768311e5b8401dee0c3f3e51f
Steve Martell <martell.steve@gmail.com>
2015-01-27 14:00:38 -0800

    Modifications to selectivity plots to show retention probability.

--- Rsrc/R4GMACS/LIB/plt_selex.R
+++ Rsrc/R4GMACS/LIB/plt_selex.R
@@ -7,33 +7,45 @@ plot.selex <- function( M )
 	for(i in 1:n)
 	{
 		A  <- M[[i]]
+		# capture
 		df <- data.frame(Model=names(M)[i],
-		                 M[[i]]$slx_capture)
-		colnames(df) <- c("Model","year","sex","fleet",as.character(A$mid_points))
-		df$sex = .SEX[df$sex+1]
+		         type = .TYPE[1],
+                 M[[i]]$slx_capture)
+	
+		# retained 
+		dr <- data.frame(Model=names(M)[i],
+	             type = .TYPE[2],
+                 M[[i]]$slx_retaind)	
+	
+		colnames(df) <- c("Model","type","year","sex","fleet",as.character(A$mid_points))
+		colnames(dr) <- colnames(df)
+		df$sex   = .SEX[df$sex+1]
 		df$fleet = .FLEET[df$fleet]
+		dr$sex   = .SEX[dr$sex+1]
+		dr$fleet = .FLEET[dr$fleet]
 
 		blkyr <- M[[i]]$slx_control[,12]
 		df    <- filter(df,year %in% blkyr)
+		dr    <- filter(dr,year %in% blkyr)
 
-		mdf <- rbind(mdf,melt(df,id.var=1:4))
+		mdf <- rbind(mdf,melt(df,id.var=1:5),melt(dr,id.var=1:5))
 	}
 
 
 	p <- ggplot(mdf)
 	if(.OVERLAY)
 	{
-		p <- p + geom_line(aes(as.numeric(variable),value,col=Model,linetype=factor(year)))
-		p <- p + facet_wrap(~sex+fleet)
+		p <- p + geom_line(aes(as.numeric(variable),value,col=type,linetype=factor(year)))
+		p <- p + facet_wrap(~Model+sex+fleet)
 	}
 	else
 	{
 		p <- p + geom_line(aes(as.numeric(variable),value,col=sex,linetype=factor(year)))
-		p <- p + facet_wrap(~Model+fleet)
+		p <- p + facet_wrap(~Model+fleet+type)
 	}
 	p <- p + labs(y="Selectivity",
 	              x="Mid point of size class (mm)",
-	              col="Sex",
+	              col="Type",
 	              linetype="Block Year")
 
 
