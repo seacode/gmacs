041bdcd9f069dae65173aa0b3c068e473ccb6c46
Darcy Webber <darcy@quantifish.co.nz>
2015-05-06 09:53:57 +1200

    Delete gmr_options.R

--- Rsrc/R/gmr_options.R
+++
@@ -1,8 +0,0 @@
-#' GMR options
-#'
-#' Parameters to be passed on to gmr plotting functions
-#'
-#' @param Resid do the residual CPUE plots
-#' @export
-#' 
-.gmr_options <- list(ObsPred = TRUE, Resid = TRUE)
