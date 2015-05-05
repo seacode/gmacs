e8b12fbf6e5d49f16a648352387f863ff71d9bb7
Darcy Webber <darcy@quantifish.co.nz>
2015-05-05 13:47:07 +1200

    Changed make.sh to remove .obj files

--- src/make.sh
+++ src/make.sh
@@ -6,4 +6,5 @@ cp gmacs build/debug/
 admb -f gmacs.tpl lib/tailcompression.cpp lib/nloglike.cpp lib/spr.cpp lib/multinomial.cpp lib/robust_multi.cpp lib/equilibrium.cpp
 cp gmacs build/release/
 rm gmacs gmacs.obj
+rm lib/*.obj
 sudo ln -s ~/gmacs/src/build/release/gmacs /usr/bin/gmacs
