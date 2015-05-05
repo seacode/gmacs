12c6291b33174c82dd474d87e7a633fecc672ce8
Darcy Webber <darcy@quantifish.co.nz>
2015-05-05 16:54:27 +1200

    Added some structural files to gmr package

---
+++ Rsrc/R/gmr_options.R
@@ -0,0 +1,8 @@
+#' GMR options
+#'
+#' Parameters to be passed on to gmr plotting functions
+#'
+#' @param Resid do the residual CPUE plots
+#' @export
+#' 
+.gmr_options <- list(ObsPred = TRUE, Resid = TRUE)
