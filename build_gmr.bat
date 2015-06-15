115a5f74b023f7314c5d429a6083645541a6e0cd
Darcy Webber <darcy@quantifish.co.nz>
2015-06-15 16:48:04 +1200

    fixed up merge

--- build_gmr.bat
+++
@@ -1,5 +0,0 @@
-R --vanilla < update-description.R
-rem run roxygen
-R --vanilla < roxygenize.R
-Rcmd build --force gmr
-Rcmd INSTALL --build gmr
--- update_gmr.sh
+++ update_gmr.sh
@@ -1,9 +1,4 @@
 cd gmr/
 R CMD BATCH update-description.R
 R CMD BATCH roxygenize.R
-cd ../
-R CMD build gmr
-R CMD INSTALL gmr_*.tar.gz
-rm gmr_*.tar.gz
-cd gmr
 chmod 777 DESCRIPTION
