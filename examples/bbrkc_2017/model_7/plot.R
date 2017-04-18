63f9acbf15e0446a09bb5b51badfc493aba9a2ee
jimianelli <jim.ianelli@noaa.gov>
2017-04-18 13:18:55 -0700

    updated ctl file to fix selectivity index, updated totals for bycatch in bairdi fishery

--- examples/bbrkc_2017/model_7/plot.R
+++ examples/bbrkc_2017/model_7/plot.R
@@ -9,7 +9,7 @@ require(gmr)
 .THEME    = theme_bw(base_size = 12, base_family = "")
 .OVERLAY  = TRUE
 .SEX      = c("Aggregate","Male","Female")
-.FLEET    = c("Pot","Trawl Bycatch","Tanner crab bycatch", "NMFS Trawl","BSFRF")
+.FLEET    = c("Pot","Trawl Bycatch","Bairdi fishery bycatch", "NMFS Trawl","BSFRF")
 .TYPE     = c("Retained & Discarded","Retained","Discarded")
 .SHELL    = c("Aggregate","New Shell","Old Shell")
 .MATURITY = c("Aggregate","Immature","Mature")
