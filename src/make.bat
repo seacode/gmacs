1af4b7c73724bbdd615899e809d8c2663e2b16d9
szuwalski <c.s.szuwalski@gmail.com>
2020-01-10 12:43:39 -0800

    changed names + make file

--- src/make.bat
+++ src/make.bat
@@ -2,7 +2,8 @@ if NOT EXIST build mkdir build
 if NOT EXIST build\debug mkdir build\debug
 if NOT EXIST build\release mkdir build\release
 copy lib\*.cpp
-call admb -g gmacs.tpl tailcompression.cpp nloglike.cpp spr.cpp multinomial.cpp robust_multi.cpp equilibrium.cpp dirichlet.cpp
+copy gmacsbase.tpl+personal.tpl gmacs.tpl
+call admb -g gmacs.tpl tailcompression.cpp nloglike.cpp spr.cpp multinomial.cpp robust_multi.cpp equilibrium.cpp dirichlet.cpp 
 :: g++ -c -std=c++14 -O3 -fpermissive -D_FILE_OFFSET_BITS=64 -I. -I"C:\Program Files (x86)\ADMB\include" -I"C:\Program Files (x86)\ADMB\contrib\include" -o gmacs.obj gmacs.cpp
 :: g++ -static -g -o gmacs.exe gmacs.obj "C:\Program Files (x86)\ADMB\lib\libadmb-contrib.a" tailcompression.obj nloglike.obj spr.obj multinomial.obj robust_multi.obj equilibrium.obj dirichlet.obj
 copy gmacs.exe build\debug
